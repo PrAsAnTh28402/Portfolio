df8c319edad42ef293de8c678401116b47e1c419
Prasanth <prasanth28402@gmail.com>
2025-09-07 18:01:29 +0530

    Vercel deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Important for hosting on Vercel
+  base: './', // ensures relative paths for JS, CSS, images work
+  
+  // Optional: define build options
+  build: {
+    outDir: 'dist', // default folder, Vercel expects this
+    emptyOutDir: true,
+  },
+  
+  // Optional: server settings for local dev
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
